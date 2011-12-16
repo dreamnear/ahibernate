8fe35d206f88a95f4941cf5922386275ae3c950a
slamhan1987 <slamhan1987@gmail.com>
2011-12-16 04:17:16 +0000

    added:junit

---
+++ Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/domain/TableUtilsTest.java
@@ -0,0 +1,17 @@
+package com.hrw.framework.ahibernate.test.domain;
+
+import junit.framework.Assert;
+
+import com.hrw.framework.ahibernate.table.TableUtils;
+import org.junit.Test;
+
+public class TableUtilsTest {
+	@Test
+	public void testBuildCreateTableStatements() {
+		String result = TableUtils.buildCreateTableStatements(
+				TableUtils.extractTableInfo(Book.class), false);
+		Assert.assertEquals("CREATE TABLE book (id INTEGER, , bookName TEXT)",
+				result);
+
+	}
+}
