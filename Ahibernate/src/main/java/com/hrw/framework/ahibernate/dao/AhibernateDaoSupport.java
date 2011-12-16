c1d62e6357e654cebc5032ba2f2d9be4276be2fe
slamhan1987 <slamhan1987@gmail.com>
2011-12-16 10:34:43 +0000

    refactory.

--- Ahibernate/src/main/java/com/hrw/framework/ahibernate/dao/AhibernateDaoSupport.java
+++ Ahibernate/src/main/java/com/hrw/framework/ahibernate/dao/AhibernateDaoSupport.java
@@ -1,30 +1,53 @@
 package com.hrw.framework.ahibernate.dao;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.hrw.framework.ahibernate.table.TableUtils;
 
 public class AhibernateDaoSupport<T> extends SQLiteOpenHelper {
+	private final static String TAG = "AhibernateDaoSupport";
+	private ArrayList<String> buildTableStatements = new ArrayList<String>();
+
 	public AhibernateDaoSupport(Context context, String name,
 			CursorFactory factory, int version) {
 		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
 	}
 
-	public Integer create(T object) {
-		return 0;
+	public void createTableStatements(Class<T>... clazzs) {
+		Class<T>[] clazzss = clazzs;
+		for (Class<T> clazz : clazzss) {
+			buildTableStatements.add(TableUtils.buildCreateTableStatements(TableUtils.extractTableInfo(clazz), true));
+		}
+	}
+	
+	public void dropTableStatements(Class<T>... clazzs) {
+		Class<T>[] clazzss = clazzs;
+		for (Class<T> clazz : clazzss) {
+			buildTableStatements.add(TableUtils.buildDropTableStatements(TableUtils.extractTableInfo(clazz), true));
+		}
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
+		for (String stmt : buildTableStatements) {
+			Log.i(TAG, "Create table statement:" + stmt);
+			db.execSQL(stmt);
+		}
 
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
+		for (String stmt : buildTableStatements) {
+			Log.i(TAG, "Create table statement:" + stmt);
+			db.execSQL(stmt);
+		}
 
 	}
 
--- Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/domain/Chapter.java
+++ Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/domain/Chapter.java
@@ -1,8 +1,14 @@
 package com.hrw.framework.ahibernate.test.domain;
 
-public class Chapter extends BaseDomain {
+import com.hrw.framework.ahibernate.annotation.Column;
+import com.hrw.framework.ahibernate.annotation.Id;
+import com.hrw.framework.ahibernate.annotation.Table;
 
+@Table(name = "chapter")
+public class Chapter extends BaseDomain {
+	@Id
 	private Long id;
+	@Column(name = "chapter_name")
 	private String chapterName;
 
 	public Long getId() {
