bf5af06ba2b11b6cd00df04c25b6f36170144124
slamhan1987@gmail.com <slamhan1987@gmail.com@369d135f-1af1-f8cc-0dd0-bd9fe3c45760>
2012-04-25 03:40:28 +0000

    refactory and add junit test.

--- trunk/Ahibernate/src/main/java/com/hrw/framework/ahibernate/core/AhibernateHelper.java
+++ trunk/Ahibernate/src/main/java/com/hrw/framework/ahibernate/core/AhibernateHelper.java
@@ -1,16 +1,30 @@
 
 package com.hrw.framework.ahibernate.core;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import com.hrw.framework.ahibernate.annotation.Column;
+import com.hrw.framework.ahibernate.annotation.Id;
+import com.hrw.framework.ahibernate.annotation.OneToMany;
 import com.hrw.framework.ahibernate.builder.DataBuilder;
 import com.hrw.framework.ahibernate.builder.InsertData;
 import com.hrw.framework.ahibernate.table.TableUtils;
 
 public abstract class AhibernateHelper<T> extends SQLiteOpenHelper {
+    private static String TAG = "AhibernateHelper";
+
+    private Boolean DEBUG = true;
 
     /**
      * Create a helper object to create, open, and/or manage a database. This
@@ -36,13 +50,18 @@ public abstract class AhibernateHelper<T> extends SQLiteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
+        if (DEBUG) {
+            Log.i(TAG, "onCreate");
+        }
         // create tables db.execSQL("");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (DEBUG) {
+            Log.i(TAG, "onUpgrade");
+        }
         // TODO Auto-generated method stub
-
     }
 
     /**
@@ -93,16 +112,108 @@ public abstract class AhibernateHelper<T> extends SQLiteOpenHelper {
         }
         return i;
     }
-//
-//    public int delete(T... objects) throws IllegalArgumentException, IllegalAccessException {
-//        SQLiteDatabase db = getWritableDatabase();
-//        int i = -1;
-//        for (Object object : objects) {
-//            InsertData insertData = DataBuilder.buildInsertSql(object);
-//            db.execSQL(insertData.getInsertSql(), insertData.getColumnAndValue().values().toArray());
-//            i++;
-//        }
-//        return i;
-//    }
+
+    public int update(T object) {
+        SQLiteDatabase db = getWritableDatabase();
+        int i = -1;
+        String tableName = TableUtils.extractTableName(object.getClass());
+        HashMap<String, String> needUpdate = new HashMap<String, String>();
+        HashMap<String, String> where = new HashMap<String, String>();
+        String updateSql = DataBuilder.buildUpdateSql(tableName, needUpdate, where);
+        db.execSQL(updateSql);
+        i++;
+        return i;
+    }
+
+    public List<T> get(Class<T> clazz, String fieldName, String fieldValue) {
+        List<T> queryList = new ArrayList<T>();
+        SQLiteDatabase db = getReadableDatabase();
+        String tableName = TableUtils.extractTableName(clazz);
+        String sql = DataBuilder.buildQueryByFieldSql(tableName, fieldName, fieldValue);
+        Cursor cursor = db.rawQuery(sql, null);
+        Field[] fields = clazz.getDeclaredFields();
+        if (cursor.moveToFirst()) {
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToPosition(i);
+                try {
+                    T t = clazz.newInstance();
+                    Annotation[] fieldAnnotations = null;
+                    for (Field field : fields) {
+                        field.setAccessible(true);
+                        fieldAnnotations = field.getAnnotations();
+                        if (fieldAnnotations.length != 0) {
+                            for (Annotation annotation : fieldAnnotations) {
+                                String columnName = null;
+                                if (annotation instanceof Id) {
+                                    columnName = ((Id) annotation).name();
+                                } else if (annotation instanceof Column) {
+                                    columnName = ((Column) annotation).name();
+                                } else if (annotation instanceof OneToMany) {
+                                    continue;
+                                    // Ignore
+                                }
+                                if (field.getType().getSimpleName().equals("Long")) {
+                                    field.set(
+                                            t,
+                                            cursor.getLong(cursor
+                                                    .getColumnIndexOrThrow((columnName != null && !columnName
+                                                            .equals("")) ? columnName : field
+                                                            .getName())));
+                                } else if (field.getType().getSimpleName().equals("String")) {
+                                    field.set(
+                                            t,
+                                            cursor.getString(cursor
+                                                    .getColumnIndexOrThrow((columnName != null && !columnName
+                                                            .equals("")) ? columnName : field
+                                                            .getName())));
+                                }
+
+                            }
+                        }
+                    }
+                    queryList.add(t);
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+        cursor.close();
+        return queryList;
+    }
+
+    /**
+     * delete entity.
+     * 
+     * @param t
+     * @return int
+     */
+    public int delete(T t) {
+        SQLiteDatabase db = getWritableDatabase();
+        String tableName = TableUtils.extractTableName(t.getClass());
+        String filedName = TableUtils.extractIdField(t.getClass()).getName();
+        String fieldValue;
+        try {
+            fieldValue = TableUtils.getFieldValue(filedName, t).toString();
+            return db.delete(tableName, filedName + " = ?", new String[] {
+                fieldValue
+            });
+        } catch (IllegalArgumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return 0;
+    }
 
 }
