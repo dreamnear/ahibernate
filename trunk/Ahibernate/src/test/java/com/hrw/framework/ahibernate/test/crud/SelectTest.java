6630b9e3bd6746b11b5162146f0543bce70bbefa
slamhan1987@gmail.com <slamhan1987@gmail.com@369d135f-1af1-f8cc-0dd0-bd9fe3c45760>
2012-04-26 15:42:21 +0000

    refactory and add junit test.

--- trunk/Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/crud/SelectTest.java
+++ trunk/Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/crud/SelectTest.java
@@ -1,6 +1,18 @@
+
 package com.hrw.framework.ahibernate.test.crud;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.hrw.framework.ahibernate.sql.Select;
+import com.hrw.framework.ahibernate.test.domain.Book;
+
 public class SelectTest {
-    public void testSelect
+    @Test
+    public void testGetTableName() {
+        Book book = new Book();
+        Select select = new Select(book);
+        Assert.assertEquals("book", select.getTableName());
+    }
 
 }
