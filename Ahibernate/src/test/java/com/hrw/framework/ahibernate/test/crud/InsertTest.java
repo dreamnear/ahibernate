1402e6da412e267983cd6df02c7e303d6aee0a30
slamhan1987 <slamhan1987@gmail.com>
2012-04-27 07:31:58 +0000

    add insert and junit test.

--- Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/crud/InsertTest.java
+++ Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/crud/InsertTest.java
@@ -8,6 +8,9 @@ import com.hrw.framework.ahibernate.sql.Insert;
 import com.hrw.framework.ahibernate.test.domain.Book;
 
 public class InsertTest {
+
+    private String EXPECTED_INSERT_SQL = "INSERT INTO book (id, book_name) values (1, newbook)";
+
     @Test
     public void testGetTableName() {
         Book book = new Book();
@@ -21,6 +24,15 @@ public class InsertTest {
         book.setBookName("newbook");
         Insert insert = new Insert(book);
         Assert.assertEquals("newbook", insert.getInsertColumns().get("book_name"));
+    }
+
+    @Test
+    public void testToStatementString() throws IllegalArgumentException, IllegalAccessException {
+        Book book = new Book();
+        book.setBookName("newbook");
+        book.setId(1L);
+        Insert insert = new Insert(book);
+        Assert.assertEquals(EXPECTED_INSERT_SQL, insert.toStatementString());
     }
 
 }
