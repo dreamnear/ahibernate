66b1d86ca6549ef3850c6ad4886e1a9c0dd4c97a
slamhan1987 <slamhan1987@gmail.com>
2012-04-27 07:49:34 +0000

    add select and junit test.

--- Ahibernate/src/main/java/com/hrw/framework/ahibernate/sql/Select.java
+++ Ahibernate/src/main/java/com/hrw/framework/ahibernate/sql/Select.java
@@ -1,6 +1,9 @@
 
 package com.hrw.framework.ahibernate.sql;
 
+import java.util.Map;
+
+import com.hrw.framework.ahibernate.builder.DataBuilder;
 import com.hrw.framework.ahibernate.table.TableUtils;
 
 public class Select {
@@ -8,13 +11,25 @@ public class Select {
 
     private String tableName;
 
+    private Map<String, String> where;
+
     public Select(Object entity) {
         this.entity = entity;
         this.tableName = TableUtils.extractTableName(entity.getClass());
     }
 
+    public Select(Object entity, Map<String, String> where) {
+        this.entity = entity;
+        this.tableName = TableUtils.extractTableName(entity.getClass());
+        this.where = where;
+    }
+
     public String getTableName() {
         return tableName;
+    }
+
+    public String toStatementString() {
+        return DataBuilder.buildSelectSql(tableName, where);
     }
 
 }
--- Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/crud/SelectTest.java
+++ Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/crud/SelectTest.java
@@ -1,6 +1,9 @@
 
 package com.hrw.framework.ahibernate.test.crud;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,6 +11,12 @@ import com.hrw.framework.ahibernate.sql.Select;
 import com.hrw.framework.ahibernate.test.domain.Book;
 
 public class SelectTest {
+    String EXPECTED_SELECT_SQL1 = "SELECT * FROM book";
+
+    String EXPECTED_SELECT_SQL2 = "SELECT * FROM book WHERE id = 1";
+
+    String EXPECTED_SELECT_SQL3 = "SELECT * FROM book WHERE id = 1 AND book_name = newbook";
+
     @Test
     public void testGetTableName() {
         Book book = new Book();
@@ -15,4 +24,29 @@ public class SelectTest {
         Assert.assertEquals("book", select.getTableName());
     }
 
+    @Test
+    public void testToStatementString1() throws IllegalArgumentException, IllegalAccessException {
+        Book book = new Book();
+        Select select = new Select(book);
+        Assert.assertEquals(EXPECTED_SELECT_SQL1, select.toStatementString());
+    }
+
+    @Test
+    public void testToStatementString2() throws IllegalArgumentException, IllegalAccessException {
+        Book book = new Book();
+        Map<String, String> where = new HashMap<String, String>();
+        where.put("id", "1");
+        Select select = new Select(book, where);
+        Assert.assertEquals(EXPECTED_SELECT_SQL2, select.toStatementString());
+    }
+    
+    @Test
+    public void testToStatementString3() throws IllegalArgumentException, IllegalAccessException {
+        Book book = new Book();
+        Map<String, String> where = new HashMap<String, String>();
+        where.put("id", "1");
+        where.put("book_name", "newbook");
+        Select select = new Select(book, where);
+        Assert.assertEquals(EXPECTED_SELECT_SQL3, select.toStatementString());
+    }
 }
