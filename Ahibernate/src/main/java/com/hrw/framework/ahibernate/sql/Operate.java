a9ef2dcb365a4f17f46d710f3b6a2ec31631728c
slamhan1987 <slamhan1987@gmail.com>
2012-04-28 09:19:55 +0000

    do some refactory.

--- Ahibernate/src/main/java/com/hrw/framework/ahibernate/sql/Operate.java
+++ Ahibernate/src/main/java/com/hrw/framework/ahibernate/sql/Operate.java
@@ -1,11 +1,17 @@
 
 package com.hrw.framework.ahibernate.sql;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.hrw.framework.ahibernate.annotation.Column;
+import com.hrw.framework.ahibernate.annotation.Id;
+import com.hrw.framework.ahibernate.annotation.OneToMany;
 import com.hrw.framework.ahibernate.annotation.Table;
 import com.hrw.framework.ahibernate.dao.AhibernatePersistence;
 
@@ -148,6 +154,51 @@ public class Operate {
             }
         }
         return buf.toString();
+    }
+
+    public Map<String, String> buildWhere(Object entity) throws Exception {
+        Map<String, String> where = new HashMap<String, String>();
+        Class clazz = entity.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+        Annotation[] fieldAnnotations = null;
+        for (Field field : fields) {
+            field.setAccessible(true);
+            fieldAnnotations = field.getAnnotations();
+            if (fieldAnnotations.length != 0) {
+                for (Annotation annotation : fieldAnnotations) {
+                    String columnName = null;
+                    if (annotation instanceof Id) {
+                        if (((Id) annotation).autoGenerate()) {
+                            continue;
+                        } else {
+                            columnName = ((Id) annotation).name();
+                        }
+                    } else if (annotation instanceof Column) {
+                        columnName = ((Column) annotation).name();
+                    } else if (annotation instanceof OneToMany) {
+                        continue;
+                        // Ignore
+                    }
+                    try {
+                        if (field.get(entity).toString().length() > 0) {
+                            where.put((columnName != null && !columnName.equals("")) ? columnName
+                                    : field.getName(), field.get(entity).toString());
+                        }
+                    } catch (IllegalArgumentException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+        if (where.isEmpty()) {
+            throw new Exception("can't delete,entity is illegal");
+        }
+        return where;
     }
 
 }
