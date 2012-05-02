911f2c858f842b10b7d2a46f2e4b037b0ad29466
slamhan1987 <slamhan1987@gmail.com>
2012-05-02 02:13:41 +0000

    do some refactory.

--- Ahibernate/src/main/java/com/hrw/framework/ahibernate/dao/AhibernateDao.java
+++ Ahibernate/src/main/java/com/hrw/framework/ahibernate/dao/AhibernateDao.java
@@ -1,10 +1,6 @@
 
 package com.hrw.framework.ahibernate.dao;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -13,9 +9,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteStatement;
 import android.util.Log;
 
-import com.hrw.framework.ahibernate.annotation.Column;
-import com.hrw.framework.ahibernate.annotation.Id;
-import com.hrw.framework.ahibernate.annotation.OneToMany;
+import com.hrw.framework.ahibernate.builder.EntityBuilder;
 import com.hrw.framework.ahibernate.sql.Delete;
 import com.hrw.framework.ahibernate.sql.Insert;
 import com.hrw.framework.ahibernate.sql.Select;
@@ -51,66 +45,17 @@ public class AhibernateDao<T> {
 
     public List<T> queryList(T entity, Map<String, String> where) {
         String sql = new Select(entity, where).toStatementString();
-        Log.d(TAG, "query sql:"+sql);
-        List<T> queryList = new ArrayList<T>();
+        Log.d(TAG, "query sql:" + sql);
         Cursor cursor = db.rawQuery(sql, null);
-        Class clazz = entity.getClass();
-        Field[] fields = clazz.getDeclaredFields();
-        if (cursor.moveToFirst()) {
-            for (int i = 0; i < cursor.getCount(); i++) {
-                cursor.moveToPosition(i);
-                try {
-                    T t = (T) clazz.newInstance();
-                    Annotation[] fieldAnnotations = null;
-                    for (Field field : fields) {
-                        field.setAccessible(true);
-                        fieldAnnotations = field.getAnnotations();
-                        if (fieldAnnotations.length != 0) {
-                            for (Annotation annotation : fieldAnnotations) {
-                                String columnName = null;
-                                if (annotation instanceof Id) {
-                                    columnName = ((Id) annotation).name();
-                                } else if (annotation instanceof Column) {
-                                    columnName = ((Column) annotation).name();
-                                } else if (annotation instanceof OneToMany) {
-                                    continue;
-                                    // Ignore
-                                }
-                                if (field.getType().getSimpleName().equals("Long")) {
-                                    field.set(
-                                            t,
-                                            cursor.getLong(cursor
-                                                    .getColumnIndexOrThrow((columnName != null && !columnName
-                                                            .equals("")) ? columnName : field
-                                                            .getName())));
-                                } else if (field.getType().getSimpleName().equals("String")) {
-                                    field.set(
-                                            t,
-                                            cursor.getString(cursor
-                                                    .getColumnIndexOrThrow((columnName != null && !columnName
-                                                            .equals("")) ? columnName : field
-                                                            .getName())));
-                                }
-
-                            }
-                        }
-                    }
-                    queryList.add(t);
-                } catch (InstantiationException e) {
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
+        EntityBuilder<T> builder = new EntityBuilder<T>(entity, cursor);
+        List<T> queryList = builder.buildQueryList();
         cursor.close();
         return queryList;
     }
 
     public void update(T entity, Map<String, String> where) {
         String sql = new Update(entity, where).toStatementString();
-        Log.d(TAG, "update sql:"+sql);
+        Log.d(TAG, "update sql:" + sql);
         SQLiteStatement stmt = null;
         try {
             stmt = db.compileStatement(sql);
@@ -126,7 +71,7 @@ public class AhibernateDao<T> {
 
     public void delete(T entity, Map<String, String> where) {
         String sql = new Delete(entity, where).toStatementString();
-        Log.d(TAG, "delete sql:"+sql);
+        Log.d(TAG, "delete sql:" + sql);
         SQLiteStatement stmt = null;
         try {
             stmt = db.compileStatement(sql);
