60935f5cf4ae6dfd13a12b56035e9c13fb0e23bc
slamhan1987 <slamhan1987@gmail.com>
2012-05-08 09:29:21 +0000

    refactory

--- Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/table/TableUtilsTest.java
+++ Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/table/TableUtilsTest.java
@@ -10,13 +10,28 @@ import com.hrw.framework.ahibernate.table.TableUtils;
 import com.hrw.framework.ahibernate.test.domain.Book;
 
 public class TableUtilsTest {
-//    @Test
+    @Test
+    public void testGetTableNameNotEmpty() {
+        Assert.assertEquals("book", TableUtils.getTableName(Book.class));
+    }
+
+    @Test
+    public void testGetTableNameEmpty() {
+        Assert.assertEquals("string", TableUtils.getTableName(String.class));
+    }
+
+    @Test
+    public void testGetTableColumns() {
+        Assert.assertEquals("book_name", TableUtils.getTableColumns(Book.class).get("bookName"));
+    }
+
+    // @Test
     public void testCreateTable() {
         SQLiteDatabase db = null;
         Assert.assertEquals(1, TableUtils.createTable(db, true, Book.class));
     }
 
-//    @Test
+    // @Test
     public void testDropTable() {
         SQLiteDatabase db = null;
         Assert.assertEquals(1, TableUtils.dropTable(db, Book.class));
