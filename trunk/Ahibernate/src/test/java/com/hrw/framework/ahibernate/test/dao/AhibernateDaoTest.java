2cf3a9cea3cf2dbccf6f73ad9523afa66cac8c4d
slamhan1987@gmail.com <slamhan1987@gmail.com@369d135f-1af1-f8cc-0dd0-bd9fe3c45760>
2012-09-18 14:49:28 +0000

    add for sql test.

--- trunk/Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/dao/AhibernateDaoTest.java
+++ trunk/Ahibernate/src/test/java/com/hrw/framework/ahibernate/test/dao/AhibernateDaoTest.java
@@ -1,17 +1,47 @@
 
 package com.hrw.framework.ahibernate.test.dao;
 
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import android.database.sqlite.SQLiteDatabase;
 
 import com.hrw.framework.ahibernate.dao.AhibernateDao;
+import com.xtremelabs.robolectric.RobolectricTestRunner;
+import com.xtremelabs.robolectric.shadows.ShadowSQLiteDatabase;
 
+@RunWith(RobolectricTestRunner.class)
 public class AhibernateDaoTest {
-//    @Test
-    public void testInsert() {
-        SQLiteDatabase db = SQLiteDatabase.create(null);
-        AhibernateDao dao = new AhibernateDao(db);
+    private AhibernateDao<?> dao;
+
+    protected SQLiteDatabase database;
+
+    protected ShadowSQLiteDatabase shDatabase;
+
+    @SuppressWarnings("rawtypes")
+    @Before
+    public void setUp() {
+        database = SQLiteDatabase.openDatabase("path", null, 0);
+        dao = new AhibernateDao(database);
+    }
+
+    @Test
+    public void should_not_null() throws Exception {
+        assertNotNull(dao);
+    }
+
+    @Test
+    public void should_return_sqlitedatabase() throws Exception {
+        assertThat(dao.getSQLiteDatabase(), notNullValue());
+    }
+
+    @Test
+    public void should_return_true_when_create_db_sql() throws Exception {
 
     }
 }
